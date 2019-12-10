5b1485ed1ff3d489885e8c4024fe47993ebb5d1c
zyspec <owners@zyspec.com>
2019-12-09 22:24:08 -0600

    Create Stats & StatsHandler classes

---
+++ class/StatsHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace XoopsModules\Tag;
+
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * XOOPS tag management module
+ *
+ * @package         XoopsModules\Tag
+ * @copyright       {@link http://sourceforge.net/projects/xoops/ The XOOPS Project}
+ * @license         {@link http://www.fsf.org/copyleft/gpl.html GNU public license}
+ * @author          Taiwen Jiang <phppp@users.sourceforge.net>
+ * @since           1.00
+ */
+
+use XoopsModules\Tag;
+
+defined('XOOPS_ROOT_PATH') || die('Restricted access');
+
+/**
+ * Tag stats handler class.
+ *
+ * {@link XoopsPersistableObjectHandler}
+ */
+class StatsHandler extends \XoopsPersistableObjectHandler
+{
+    /**
+     * StatsHandler constructor.
+     * @param \XoopsDatabase|null $db
+     */
+    public function __construct(\XoopsDatabase $db = null)
+    {
+        parent::__construct($db, 'tag_stats', Stats::class, 'ts_id');
+    }
+}
